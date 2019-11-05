a49c50b3aa2c8f4e3b974bfb5ee5db7660ea7b8d
gabrypol <gabrielepolenta@gmail.com>
2019-11-05 07:46:12 +0100

    Updates

---
+++ precache-manifest.01bb21877be4cbd41d6d40097561641f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d4aafd7612de24c756bac19892f8a1",
+    "url": "/Address-Book-App/index.html"
+  },
+  {
+    "revision": "efe36d6fee4860834d93",
+    "url": "/Address-Book-App/static/css/main.8979bf4e.chunk.css"
+  },
+  {
+    "revision": "6f3333b3858288e159b4",
+    "url": "/Address-Book-App/static/js/2.288cf491.chunk.js"
+  },
+  {
+    "revision": "efe36d6fee4860834d93",
+    "url": "/Address-Book-App/static/js/main.73ef0367.chunk.js"
+  },
+  {
+    "revision": "3084b342f87c09f3f7bb",
+    "url": "/Address-Book-App/static/js/runtime-main.e0a5aa86.js"
+  }
+]);
